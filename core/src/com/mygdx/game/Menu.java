484627cdc412ab258d715d2ab67acb797859f965
A-O-M <aaronobando2007@gmail.com>
2023-08-05 01:15:01 -0600

    Juego

--- core/src/com/mygdx/game/Menu.java
+++ core/src/com/mygdx/game/Menu.java
@@ -1,66 +1,7 @@
 package com.mygdx.game;
 
-public class Menu {public static void main(String[] args) {
-    Scanner sn = new Scanner(System.in);
+public class Menu {
 
-    boolean salir = false;
-    int opcion;
-
-    while(!salir){
-
-       System.out.println("\n\t-- Programa que muestra un menu con opciones de ejercicios de las diferentes estructuras --");
-
-       System.out.println("1.- Funciones");
-       System.out.println("2.- Recursividad");
-       System.out.println("3.- Metodo de ordenacion de insercion");
-       System.out.println("4.- Metodo de ordenacion Shell");
-       System.out.println("5.- Listas de personas");
-       System.out.println("6.- Pilas");
-       System.out.println("7.- Colas");
-       System.out.println("8.- Salir");
-
-       try{
-
-          System.out.println("Seleccione la opcion deseada: ");
-          opcion = sn.nextInt();
-
-          switch (opcion){
-          case 1:
-             Ejercicios.funciones();
-             break;
-          case 2:
-             Ejercicios.recursividad();
-             break;
-          case 3:
-             Ejercicios.ordenacionInsercion();
-             break;
-          case 4:
-             Ejercicios.ordenacionShell();
-             break;
-          case 5:
-             Ejercicios.listaPersonas();
-             break;
-          case 6:
-             Ejercicios.metodoPilas();
-             break;
-          case 7:
-             Ejercicios.metodoColas();
-             break;
-          case 8:
-             salir=true;
-             break;   
-          default:   
-             System.out.println("Las opciones son entre 1 y 8");
-
-          }
-       }
-       catch(InputMismatchException e){
-          System.out.println("Debes seleccionar un numero de opcion");
-          sn.next();
-       }
-    }
-
-    System.out.println("Gracias por utilizar el sistema, hasta luego... ");
- }
+   
    
 }
---
+++ core/src/com/mygdx/game/screens.java
@@ -0,0 +1,20 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.Screen;
+
+public abstract class screens extends InputAdapter implements Screen {
+   public static final float  SCREEN_WIDTH = 480;
+   public static final float  SCREEN_HEIGHT = 800;
+
+   public static final float  WORLD_HEIGHT = 8f;
+   public static final float  WORLD_WIDTH = 4.8f;
+
+   
+      
+
+
+
+
+    
+}
